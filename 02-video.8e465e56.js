2f133f58d7a19c119bd8928984d4fb22c94edda8
SkybaMaryna <122438581+SkybaMaryna@users.noreply.github.com>
2023-03-19 14:47:27 +0000

    Deploying to gh-pages from @ SkybaMaryna/goit-js-hw-08@7bf367ad29153206b955c836e400d4c8e1dcfa15 🚀

---
+++ 02-video.8e465e56.js
@@ -0,0 +1,2 @@
+new Vimeo.Player("#vimeo-player").on("play",(function(){console.log("played the video!")}));
+//# sourceMappingURL=02-video.8e465e56.js.map
---
+++ 02-video.9769d9ff.js
@@ -0,0 +1,2 @@
+new Vimeo.Player("#vimeo-player").on("play",(function(){console.log("played the video!")}));
+//# sourceMappingURL=02-video.9769d9ff.js.map
