8fe6cd9109f57f8f2ddc74991ea3cbc20f30cdc0
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-03-20 17:00:22 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const throttle = require('lodash.throttle');
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+}
+
+refs.form.addEventListener('input', throttle(onSetData, 500))
+refs.form.addEventListener('submit', onSubmitForm)
+
+if (localStorage.getItem("feedback-form-state")) {
+    refs.input.value = JSON.parse(localStorage.getItem("feedback-form-state")).email
+    refs.textarea.value = JSON.parse(localStorage.getItem("feedback-form-state")).message
+}
+    
+function onSetData(event) {
+      let inputData = {
+        email: event.target.form[0].value,
+        message: event.target.form[1].value,
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(inputData));
+}
+
+function onSubmitForm(event) {
+    event.preventDefault();
+
+    refs.form.reset();
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    localStorage.removeItem("feedback-form-state");
+}
