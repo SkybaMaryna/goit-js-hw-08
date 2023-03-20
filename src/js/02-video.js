e4bc8aa4dd6c438a9b83b48ba6de217cc0774322
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-03-20 14:39:18 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,14 @@
 import Vimeo from "@vimeo/player";
 const throttle = require('lodash.throttle');
 
-
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
+
+player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+}, 1000));
 
 const savedSettings = localStorage.getItem("videoplayer-current-time");
 const parsedSettings = JSON.parse(savedSettings);
 
 player.setCurrentTime(parsedSettings.seconds)
-
-player.on('timeupdate', throttle(function(data) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
-}, 1000));
